7bf898bc15856f543049afb8aeec7b5c5f2e9829
Sunny Patel <sunny33p@gmail.com>
2020-02-07 12:29:20 +1100

    Initial Commit

---
+++ StackViewsExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackViewsExample
+//
+//  Created by Sunny Patel on 7/2/20.
+//  Copyright © 2020 Sunny Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
